ad145a85b3e7ae5ca4d77806b988e9cf06462139
Reeex <32922132+Reeex@users.noreply.github.com>
2025-04-09 14:50:58 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,8 @@
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
-  );
+self.addEventListener('install', (e) => {
+  console.log('Service Worker 安裝完成');
 });
+
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
 });
+    
